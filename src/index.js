c095333aaa733ce35b50e3998140938eb326fc18
ykruk <yulka.kr@gmail.com>
2020-02-21 22:30:14 +0300

    feat: add typical-arrays-problems solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(!array || array.length === 0) return 0;
+ 	return array.reduce((acc, el) => {
+        return el < acc ? el : acc;
+    }, 0);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(!array || array.length === 0) return 0;
+    return array.reduce((acc, el) => {
+        return el > acc ? el : acc;
+    }, 0);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!array || array.length === 0) return 0;
+    const res = array.reduce((acc, el) => {
+		acc.sum += el;
+		acc.count++;
+		return acc;
+	}, {sum: 0, count: 0});
+	return res.sum / res.count;
 }
