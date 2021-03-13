39246b64c8290212a906b93637197b2a87cd07dd
ykruk <yuliyakruk@tut.by>
2021-03-13 22:36:29 +0300

    refactor: shorten returns

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 
 exports.min = function min (array) {
     if(!array || array.length === 0) return 0;
- 	return array.reduce((acc, el) => {
-        return el < acc ? el : acc;
-    }, 0);
+ 	return array.reduce((acc, el) => el < acc ? el : acc, 0);
 }
 
 exports.max = function max (array) {
     if(!array || array.length === 0) return 0;
-    return array.reduce((acc, el) => {
-        return el > acc ? el : acc;
-    }, 0);
+    return array.reduce((acc, el) => el > acc ? el : acc, 0);
 }
 
 exports.avg = function avg (array) {
     if(!array || array.length === 0) return 0;
-    const res = array.reduce((acc, el) => {
-		acc.sum += el;
-		acc.count++;
-		return acc;
-	}, {sum: 0, count: 0});
-	return res.sum / res.count;
+    return res = array.reduce((acc, el) => acc += el, 0) / array.length;
 }
